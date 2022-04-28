fb51c54be75319e04132f9668d3c39ebdc683451
ravilella1234 <masters.selenium@gmail.com>
2022-04-28 08:54:35 +0530

    Downloads By uing Options

--- April2022_8AM_seleniumBatch/src/test/java/autoit/FileDownloadUsingOptions.java
+++ April2022_8AM_seleniumBatch/src/test/java/autoit/FileDownloadUsingOptions.java
@@ -4,56 +4,76 @@ import java.util.HashMap;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.ProfilesIni;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FileDownloadUsingOptions 
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws Exception 
 	{
-		String fileType = "DOC, DOCX";
+		//http://kb.mozillazine.org/About:config_entries
+		
+		String fileType = "PDF";
+		WebDriver driver;
 		
 		//For Chrome Browser
 		
-		/*
-		 * WebDriverManager.chromedriver().setup(); ChromeOptions option = new
-		 * ChromeOptions(); option.
-		 * addArguments("user-data-dir=C:\\Users\\ravi\\AppData\\Local\\Google\\Chrome\\User Data\\Default"
-		 * ); HashMap<String, Object> pref = new HashMap<String, Object>();
-		 * pref.put("plugins.always_open_pdf_externally", true);
-		 * pref.put("download.default_directory", "C:\\Users\\ravi\\files");
-		 * option.setExperimentalOption("prefs", pref);
-		 * 
-		 * WebDriver driver = new ChromeDriver(option);
-		 * driver.manage().window().maximize();
-		 * driver.get("https://file-examples.com/");
-		 */
 		
+		 WebDriverManager.chromedriver().setup(); 
+		 ChromeOptions option = new ChromeOptions(); 
+		 option.addArguments("user-data-dir=C:\\Users\\ravi\\AppData\\Local\\Google\\Chrome\\User Data\\Default"); 
+		 HashMap<String, Object> pref = new HashMap<String, Object>();
+		 pref.put("plugins.always_open_pdf_externally", true);
+		 pref.put("download.default_directory", "C:\\Users\\ravi\\files");
+		 option.setExperimentalOption("prefs", pref);
+		 driver = new ChromeDriver(option);
+		 
 		
 		
 		//For Edge Browser
 		
-		//WebDriverManager.edgedriver().setup();
-		System.setProperty("webdriver.edge.driver", "C:\\Users\\ravi\\Downloads\\edgedriver_win64\\msedgedriver.exe");
+		/*System.setProperty("webdriver.edge.driver", "C:\\Users\\ravi\\Downloads\\edgedriver_win64\\msedgedriver.exe");
 		EdgeOptions option1 = new EdgeOptions();
 		option1.addArguments("--user-data-dir=C:\\Users\\ravi\\AppData\\Local\\Microsoft\\Edge\\User Data\\Default");
 		HashMap<String, Object> pref1 = new HashMap<String, Object>();
 		pref1.put("plugins.always_open_pdf_externally", true);
 		pref1.put("download.default_directory", "C:\\Users\\ravi\\files");
 		option1.setExperimentalOption("prefs", pref1);
+		driver = new EdgeDriver(option1);*/
 		
-		WebDriver driver = new EdgeDriver(option1);
-		driver.manage().window().maximize();
-		driver.get("https://file-examples.com/");
+		
 		
 		//For Firefox Browser
-
 		
+		/*WebDriverManager.firefoxdriver().setup();
+		FirefoxOptions option = new FirefoxOptions();
+		ProfilesIni p = new ProfilesIni();
+		FirefoxProfile profile = p.getProfile("AprilFFProfile");
+				
+		profile.setPreference("pdfjs.disabled", true);
+		profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "application/pdf");
+		profile.setPreference("browser.download.dir", "C:\\Users\\ravi\\files");
+		profile.setPreference("browser.download.folderList", 2);
+		option.setProfile(profile);
+		driver = new FirefoxDriver(option);*/
+		
+		
+		driver.manage().window().maximize();
+		driver.get("https://file-examples.com/");
+		driver.manage().timeouts().getPageLoadTimeout();
+		Thread.sleep(4000);
 		
 		driver.findElement(By.xpath("//h3[text()='Documents']/following-sibling::a")).click();
+		Thread.sleep(4000);
 		driver.findElement(By.xpath("//td[text()='"+fileType+"']/following-sibling::td[2]/a")).click();
 		driver.findElement(By.xpath("//a[starts-with(text(),'Download sample')]")).click();
 	}
