d6b66b1e058c1560f46ad9e0abef7ef74b9bd923
ravilella1234 <masters.selenium@gmail.com>
2022-05-05 09:07:55 +0530

    Verifications & Reportings

---
+++ April2022_8AM_seleniumBatch/src/test/java/verificationsReportings/Reporting.java
@@ -0,0 +1,35 @@
+package verificationsReportings;
+
+import com.aventstack.extentreports.Status;
+
+import launcher.BaseTest;
+
+public class Reporting extends BaseTest
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+		init();
+		test = rep.createTest("Reporting");
+		//test.log(Status.INFO,"Init the Properties files....");
+		logInfo("Init the Properties files....");
+		
+		launch("chromebrowser");
+		logInfo("Opening the Browser :- " + p.getProperty("chromebrowser"));
+		
+		navigateUrl("amazonurl");
+		logInfo("Navigating to application : " + childProp.getProperty("amazonurl"));
+		
+		String expectedLink = "Customer Serv";
+		
+		if(!isLinkEqual(expectedLink))
+			reportFailure("Both Links are not equal...");
+		else
+			reportSuccess("Both Links are equal...");
+		
+		rep.flush();
+	}
+
+	
+
+}
