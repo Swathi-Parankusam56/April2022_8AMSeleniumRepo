74f3f57520d561e4be69d8f7b2926419cfedb14b
ravilella1234 <masters.selenium@gmail.com>
2022-06-22 07:52:32 +0530

    Project Added

---
+++ HybridNewProject/src/main/java/keywords/GenericKeywords.java
@@ -0,0 +1,36 @@
+package keywords;
+
+public class GenericKeywords 
+{
+	public void openBrowser(String nrowserName)
+	{
+		
+	}
+	
+	public void navigate(String url)
+	{
+		
+	}
+	
+	public void click(String locator)
+	{
+		
+	}
+	
+	public void type(String locator,String text)
+	{
+		
+	}
+	
+	public void select(String locator,String option)
+	{
+		
+	}
+	
+	public String getText()
+	{
+		return null;
+		
+	}
+
+}
